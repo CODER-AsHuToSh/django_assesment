21a0ed6a3041e1f5801c023e34a05969262998ad
Ashutosh gupta <ashutosh.gupta.civ20@itbhu.ac.in>
2023-12-08 13:30:41 +0530

    coverage improve part 2

--- todo/tests/unit_tests/test_serializers.py
+++ todo/tests/unit_tests/test_serializers.py
@@ -1,51 +1,45 @@
-from rest_framework.test import APITestCase
+from django.test import TestCase
 from todo.models import TodoItem, Tag
 from todo.api.serializers import TagSerializer, TodoItemSerializer
 
-class TodoItemSerializerTest(APITestCase):
+class TagSerializerTest(TestCase):
+    def test_tag_serializer(self):
+        tag_data = {'name': 'Test Tag'}
+        serializer = TagSerializer(data=tag_data)
+        self.assertTrue(serializer.is_valid())
+        tag_instance = serializer.save()
+        self.assertIsInstance(tag_instance, Tag)
+        self.assertEqual(tag_instance.name, 'Test Tag')
+
+class TodoItemSerializerTest(TestCase):
     def setUp(self):
-        self.tag1 = Tag.objects.create(name='tag1')
-        self.tag2 = Tag.objects.create(name='tag2')
+        self.tag1 = Tag.objects.create(name='Tag1')
+        self.tag2 = Tag.objects.create(name='Tag2')
 
-    def test_create_todo_item_with_tags(self):
-        # Create test data
-        tag_data = [{'name': 'tag1'}, {'name': 'tag2'}]
+    def test_todo_item_serializer_create(self):
         todo_data = {
-            'title': 'Test Todo Item',
+            'title': 'Test Todo',
             'description': 'Test Description',
-            'tags': tag_data
+            'tags': [{'name': 'Tag1'}, {'name': 'Tag2'}]
         }
-
-        # Serialize and validate data
         serializer = TodoItemSerializer(data=todo_data)
         self.assertTrue(serializer.is_valid())
+        todo_instance = serializer.save()
+        self.assertIsInstance(todo_instance, TodoItem)
+        self.assertEqual(todo_instance.title, 'Test Todo')
+        self.assertEqual(todo_instance.description, 'Test Description')
+        self.assertEqual(todo_instance.tags.count(), 2)
 
-        # Call create method and check for successful creation
-        todo_item = serializer.save()
-        self.assertIsInstance(todo_item, TodoItem)
-        self.assertEqual(todo_item.title, 'Test Todo Item')
-
-        # Check tags are associated properly
-        self.assertEqual(todo_item.tags.count(), 2)
-
-    def test_create_todo_item_with_existing_tags(self):
-        # Create test data with existing tags
-        tag_data = [{'name': 'tag1'}, {'name': 'tag2'}]
+    def test_todo_item_serializer_update(self):
+        todo_item = TodoItem.objects.create(title='Existing Todo', description='Existing Description')
         todo_data = {
-            'title': 'Todo with existing tags',
-            'description': 'Description with existing tags',
-            'tags': tag_data
+            'title': 'Updated Todo',
+            'description': 'Updated Description',
+            'tags': [{'name': 'Tag1'}, {'name': 'Tag3'}]  # 'Tag3' doesn't exist yet
         }
-
-        # Serialize and validate data
-        serializer = TodoItemSerializer(data=todo_data)
+        serializer = TodoItemSerializer(instance=todo_item, data=todo_data, partial=True)
         self.assertTrue(serializer.is_valid())
-
-        # Call create method and check for successful creation
-        todo_item = serializer.save()
-        self.assertIsInstance(todo_item, TodoItem)
-        self.assertEqual(todo_item.title, 'Todo with existing tags')
-
-        # Check tags are associated properly
-        self.assertEqual(todo_item.tags.count(), 2)
-
+        updated_todo = serializer.save()
+        self.assertEqual(updated_todo.title, 'Updated Todo')
+        self.assertEqual(updated_todo.description, 'Updated Description')
+        self.assertEqual(updated_todo.tags.count(), 2)  # One existing and one newly created tag
